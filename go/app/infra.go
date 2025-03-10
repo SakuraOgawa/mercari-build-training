7cddb764745dbb2aa797f6c55d1a4154539b1f6b
Sakura <g23934os@gm.tsuda.ac.jp>
2025-03-10 21:38:51 +0900

    update STEP4

--- go/app/infra.go
+++ go/app/infra.go
@@ -11,11 +11,13 @@ import (
 )
 
 var errImageNotFound = errors.New("image not found")
+var errItemNotFound = errors.New("item not found")
 
 type Item struct {
 	ID       int    `db:"id" json:"-"`
 	Name     string `db:"name" json:"name"`
 	Category string `db:"category" json:"category"`
+	Image    string `db:"image" json:"image"`
 }
 
 // Please run `go generate ./...` to generate the mock implementation
@@ -24,6 +26,8 @@ type Item struct {
 //go:generate go run go.uber.org/mock/mockgen -source=$GOFILE -package=${GOPACKAGE} -destination=./mock_$GOFILE
 type ItemRepository interface {
 	Insert(ctx context.Context, item *Item) error
+	List(ctx context.Context) ([]*Item, error)
+	Select(ctx context.Context, id int) ([]*Item, error)
 }
 
 // itemRepository is an implementation of ItemRepository
@@ -47,13 +51,11 @@ func (i *itemRepository) Insert(ctx context.Context, item *Item) error {
 
 	oldData, err := os.ReadFile(i.fileName)
 	if err != nil && !os.IsNotExist(err) {
-		fmt.Println("Failed to read file:", err)
 		return fmt.Errorf("failed to read file: %w", err)
 	}
 
 	if len(oldData) > 0 {
 		if err := json.Unmarshal(oldData, &data); err != nil {
-			fmt.Println("Failed to unmarshal JSON:", err)
 			return fmt.Errorf("failed to unmarshal JSON: %w", err)
 		}
 	}
@@ -63,12 +65,10 @@ func (i *itemRepository) Insert(ctx context.Context, item *Item) error {
 
 	newData, err := json.MarshalIndent(data, "", "  ")
 	if err != nil {
-		fmt.Println("Failed to marshal JSON:", err)
 		return fmt.Errorf("failed to marshal JSON: %w", err)
 	}
 
 	if err := os.WriteFile(i.fileName, newData, 0644); err != nil {
-		fmt.Println("Failed to write file:", err)
 		return fmt.Errorf("failed to write file: %w", err)
 	}
 
@@ -80,6 +80,44 @@ func (i *itemRepository) Insert(ctx context.Context, item *Item) error {
 // This package doesn't have a related interface for simplicity.
 func StoreImage(fileName string, image []byte) error {
 	// STEP 4-4: add an implementation to store an image
+	//画像を保存する
+	if err := os.WriteFile(fileName, image, 0644); err != nil {
+		return fmt.Errorf("failed to image file: %w", err)
+	}
 
 	return nil
+}
+
+func (i *itemRepository) List(ctx context.Context) ([]*Item, error) {
+	fileData, err := os.ReadFile(i.fileName)
+	if err != nil && !os.IsNotExist(err) {
+		return nil, fmt.Errorf("failed to read file: %w", err)
+	}
+
+	var jsonData struct {
+		Items []*Item `json:"items"`
+	}
+
+	if err := json.Unmarshal(fileData, &jsonData); err != nil {
+		return nil, fmt.Errorf("failed to unmashal JSON: %w", err)
+	}
+	return jsonData.Items, nil
+}
+
+func (i *itemRepository) Select(ctx context.Context, id int) ([]*Item, error) {
+	if id <= 0 {
+		return nil, errItemNotFound
+	}
+
+	items, err := i.List(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(items) < id {
+		return nil, errItemNotFound
+	}
+
+	return []*Item{items[id-1]}, nil
+
 }
